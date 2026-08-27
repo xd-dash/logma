db241fcfb51169d66c152cc18e67690e1cfee572
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2026-08-26 22:30:36 -0700

    Isolate legacy Redis DB tests from scope override

--- internal/routes/redis_config_test.go
+++ internal/routes/redis_config_test.go
@@ -8,6 +8,7 @@ func TestRedisOptionsFromEnvDefaultsToTCP(t *testing.T) {
 	t.Setenv("REDIS_URI", "redis.example:6379")
 	t.Setenv("REDISCLI_AUTH", "secret")
 	t.Setenv("REDIS_USERNAME", "logma")
+	t.Setenv("LOGMA_SCOPE_DB", "")
 	t.Setenv("REDIS_DB", "3")
 
 	opts := redisOptionsFromEnv()
@@ -20,6 +21,7 @@ func TestRedisOptionsFromEnvSocketWins(t *testing.T) {
 	t.Setenv("REDIS_NETWORK", "tcp")
 	t.Setenv("REDIS_URI", "ignored:6379")
 	t.Setenv("REDIS_SOCKET", "/run/logma/redis.sock")
+	t.Setenv("LOGMA_SCOPE_DB", "")
 	t.Setenv("REDIS_DB", "0")
 
 	opts := redisOptionsFromEnv()
