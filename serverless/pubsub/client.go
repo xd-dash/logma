edcbd8729d50a621d3709c0555db157b7f8d7d71
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2026-08-31 03:18:12 -0700

    Support Redis ACL worker usernames

--- serverless/pubsub/client.go
+++ serverless/pubsub/client.go
@@ -8,37 +8,28 @@ import (
 )
 
 const HeaderRedisAuth = "X-Rediscli-Auth"
+const HeaderRedisUsername = "X-Redis-Username"
 const HeaderRedisURI = "X-Redis-Uri"
 const HeaderRedisSocket = "X-Redis-Socket"
 
 func NewClientFromEnv() *redis.Client {
-	return newClient(os.Getenv("REDIS_URI"), os.Getenv("REDIS_SOCKET"), os.Getenv("REDISCLI_AUTH"))
+	return newClient(os.Getenv("REDIS_URI"), os.Getenv("REDIS_SOCKET"), os.Getenv("REDIS_USERNAME"), os.Getenv("REDISCLI_AUTH"))
 }
 
 func NewClientFromRequest(r *http.Request) *redis.Client {
 	addr := os.Getenv("REDIS_URI")
-	if addr == "" {
-		addr = r.Header.Get(HeaderRedisURI)
-	}
-
+	if addr == "" { addr = r.Header.Get(HeaderRedisURI) }
 	socket := os.Getenv("REDIS_SOCKET")
-	if socket == "" {
-		socket = r.Header.Get(HeaderRedisSocket)
-	}
-
+	if socket == "" { socket = r.Header.Get(HeaderRedisSocket) }
+	username := os.Getenv("REDIS_USERNAME")
+	if username == "" { username = r.Header.Get(HeaderRedisUsername) }
 	auth := os.Getenv("REDISCLI_AUTH")
-	if auth == "" {
-		auth = r.Header.Get(HeaderRedisAuth)
-	}
-
-	return newClient(addr, socket, auth)
+	if auth == "" { auth = r.Header.Get(HeaderRedisAuth) }
+	return newClient(addr, socket, username, auth)
 }
 
-func newClient(addr, socket, auth string) *redis.Client {
-	opts := &redis.Options{Addr: addr, Password: auth, DB: 0}
-	if socket != "" {
-		opts.Network = "unix"
-		opts.Addr = socket
-	}
+func newClient(addr, socket, username, auth string) *redis.Client {
+	opts := &redis.Options{Addr: addr, Username: username, Password: auth, DB: 0}
+	if socket != "" { opts.Network = "unix"; opts.Addr = socket }
 	return redis.NewClient(opts)
 }
