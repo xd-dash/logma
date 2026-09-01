c47dc471f43adfa3cf79131020f8cc43cd00c077
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2026-08-31 21:52:17 -0700

    Expose 10 minute lifecycle policy

--- internal/lifecycle/http.go
+++ internal/lifecycle/http.go
@@ -48,6 +48,7 @@ func writePolicies(w http.ResponseWriter) {
 	names := []ratelimiter.LifecyclePolicyName{
 		ratelimiter.LifecycleSmoke30S,
 		ratelimiter.LifecycleSmoke1M,
+		ratelimiter.LifecycleSmoke10M,
 		ratelimiter.LifecycleSandbox1D,
 		ratelimiter.LifecycleSandbox3D,
 		ratelimiter.LifecycleSandbox7D,
