d4f1a070e6e7ba15359bac8470dd382b0c56e724
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2026-09-03 14:26:15 -0700

    Add explicit Pub/Sub resource conflict error

--- internal/pubsubmodel/errors.go
+++ internal/pubsubmodel/errors.go
@@ -4,6 +4,7 @@ import "errors"
 
 var (
 	ErrNotFound         = errors.New("pubsub resource not found")
+	ErrAlreadyExists    = errors.New("pubsub resource already exists")
 	ErrMissingReference = errors.New("pubsub resource references missing dependency")
 	ErrReferenced       = errors.New("pubsub resource is still referenced")
 )
